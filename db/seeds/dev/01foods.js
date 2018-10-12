8120d8027f7c048764d13806969b411e58ce5545
JP Lynch <JPLynch35@gmail.com>
2018-10-12 15:11:16 -0600

    Set up express and add first endpoint

---
+++ db/seeds/dev/01foods.js
@@ -0,0 +1,29 @@
+exports.seed = function(knex, Promise) {
+  // We must return a Promise from within our seed function
+  // Without this initial `return` statement, the seed execution
+  // will end before the asynchronous tasks have completed
+  // knex('mealFoods').del(); // delete all footnotes first
+  // knex('meals').del(); // delete all footnotes first
+  return knex('foods').del() // delete all footnotes first
+
+    // Now that we have a clean slate, we can re-insert our paper data
+    .then(() => {
+      return Promise.all([
+
+        // Insert a single paper, return the paper ID, insert 2 footnotes
+        knex('foods').insert(
+          { name: 'banana', calories: 150 }, 'id'),
+        knex('foods').insert(
+          { name: 'apple', calories: 100 }, 'id'),
+        knex('foods').insert(
+          { name: 'strawberries', calories: 50 }, 'id'),
+        knex('foods').insert(
+          { name: 'cherries', calories: 75 }, 'id'),
+        knex('foods').insert(
+          { name: 'meatloaf', calories: 300 }, 'id')
+        .then(() => console.log('Seeding foods complete!'))
+        .catch(error => console.log(`Error seeding foods data: ${error}`))
+      ]) // end return Promise.all
+    })
+    .catch(error => console.log(`Error seeding foods data: ${error}`));
+};
