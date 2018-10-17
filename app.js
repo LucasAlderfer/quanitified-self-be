622f831a425121a8bfc522b838f3694fd521a56c
JP Lynch <JPLynch35@gmail.com>
2018-10-17 13:13:14 -0600

    Add Cors for production

--- app.js
+++ app.js
@@ -127,6 +127,13 @@ app.delete('/api/v1/meals/:mealId/foods/:foodId', (request, response) => {
   })
 });
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "DELETE, PATCH");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
 });
